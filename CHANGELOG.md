e88d0c7b6b466b06e136dfb32820c481db8948eb
Cẩm Huỳnh <huynhquancam@gmail.com>
2024-07-18 05:33:12 +0200

    Release v0.2.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # CHANGELOG
 
+## v0.2.0
+
+- Introduce `transform/2` and `put_new/3`.
+
 ## v0.1.0
 
 Initial release
--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule Vaxin.MixProject do
   use Mix.Project
 
   @name "Vaxin"
-  @version "0.1.0"
+  @version "0.2.0"
   @source_url "https://github.com/kraftjectory/vaxin"
 
   def project() do
